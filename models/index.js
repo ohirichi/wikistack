0e5403227cccdcd52b86a796e343b3e984dd76d3
Destin McMurry <destinmcmurry@gmail.com>
2018-03-09 17:10:40 -0500

    added tables to db

--- models/index.js
+++ models/index.js
@@ -1,2 +1,14 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost/wikistack');
+
+const Page = sequelize.define('page', {
+    title: Sequelize.STRING,
+    urlTitle: Sequelize.TEXT,
+    content: Sequelize.TEXT,
+    status: Sequelize.INTEGER,
+})
+
+const User = sequelize.define('user', {
+    name: Sequelize.STRING,
+    email: Sequelize.TEXT,
+})
